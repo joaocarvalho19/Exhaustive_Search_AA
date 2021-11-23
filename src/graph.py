4e33a400a8000499684995550e99f1970501aaf9
joaocarvalho19 <joao.carvalho19@hotmail.com>
2021-11-23 00:04:22 +0000

    find min edge dom set

--- src/graph.py
+++ src/graph.py
@@ -5,6 +5,7 @@ class Graph:
     def __init__(self, num_vertices):
         self.num_vertices = num_vertices
         self.edges = []
+        self.edge_adjacency = {}
 
         # Init matrix
         self.incidence_matrix = [[] * num_vertices for n in range(0,num_vertices)]
@@ -12,22 +13,46 @@ class Graph:
 
     # Print the matrix
     def printMatriz(self):
-        print(" - Matriz: - ")
+        print(" - Incidence Matrix: - ")
         for row in self.incidence_matrix:
             print(row)
         print("-")
 
-    def getAllPairsOfEdges(self):
+    # Get all combinations
+    def getAllSolutions(self):
         all_possible_sol = []
         """zipped_rows = zip(*self.incidence_matrix)
         matriz_T = [list(row) for row in zipped_rows]"""
 
-        print(self.edges)
+        zipped_rows = zip(*self.incidence_matrix)
+        matriz_T = [list(row) for row in zipped_rows]
         for i in range(len(self.edges)):
             all_possible_sol += combinations(self.edges, i+1)
-        
 
+            adj_edges = []
+            for j in range(len(matriz_T[i])):
+                if matriz_T[i][j] == 1:
+                    adj_edges += [e for e in range(len(self.incidence_matrix[j])) if self.incidence_matrix[j][e] == 1 and e not in adj_edges and e != i]
+            
+            self.edge_adjacency[i] = adj_edges
+        
         return all_possible_sol
+
+
+    def findSolution(self, all_possible_sol):
+        res = []
+        for sol in all_possible_sol:
+
+            adj_edges = []
+            for e in sol:
+                adj_edges += self.edge_adjacency[e]
+
+            if len(adj_edges) == len(self.edges):
+                res.append(sol)
+
+
+        return min(res)
+
 
     # Add edges
     def add_edge(self, v1, v2):
@@ -45,6 +70,7 @@ class Graph:
 
         #self.incidence_matrix[v1][v2] += 1
         #self.incidence_matrix[v2][v1] += 1
+
 
 
     # Remove edges
--- src/main.py
+++ src/main.py
@@ -6,11 +6,12 @@ if __name__ == "__main__":
 
     graph = Graph(5)
     graph.add_edge(0,1)
-    graph.add_edge(0,2)
-    graph.add_edge(1,3)
+    graph.add_edge(1,2)
     graph.add_edge(2,3)
-    graph.add_edge(1,4)
+    graph.add_edge(3,4)
 
     graph.printMatriz()
 
-    print(graph.getAllPairsOfEdges())
+    all_sol = graph.getAllSolutions()
+
+    print("Minimum edge dominating set: {}".format(graph.findSolution(all_sol)))
