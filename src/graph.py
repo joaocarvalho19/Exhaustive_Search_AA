49fe3bc54e8daaaa85e4b273f5819394ae17352d
joaocarvalho19 <joao.carvalho19@hotmail.com>
2021-11-28 23:13:52 +0000

    greedy heur

--- src/graph.py
+++ src/graph.py
@@ -21,20 +21,9 @@ class Graph:
     # Get all combinations
     def getAllSolutions(self):
         all_possible_sol = []
-        """zipped_rows = zip(*self.incidence_matrix)
-        matriz_T = [list(row) for row in zipped_rows]"""
 
-        zipped_rows = zip(*self.incidence_matrix)
-        matriz_T = [list(row) for row in zipped_rows]
         for i in range(len(self.edges)):
             all_possible_sol += combinations(self.edges, i+1)
-
-            adj_edges = []
-            for j in range(len(matriz_T[i])):
-                if matriz_T[i][j] == 1:
-                    adj_edges += [e for e in range(len(self.incidence_matrix[j])) if self.incidence_matrix[j][e] == 1 and e not in adj_edges and e != i]
-            
-            self.edge_adjacency[i] = sorted(adj_edges)
         
         return all_possible_sol
 
@@ -54,7 +43,6 @@ class Graph:
     
     def findGreedySolution(self):
         sorted_dictionary = dict(sorted(self.edge_adjacency.items(), key = lambda t: len(t[1]), reverse=True))       # Sort by length of the list(edge adjacency)
-        print(sorted_dictionary)
 
         res = []
         while sorted_dictionary:
@@ -69,27 +57,34 @@ class Graph:
                 if e in sorted_dictionary.keys():
                     del sorted_dictionary[e]
             
-        print(res)
         return res
 
 
     # Add edges
     def add_edge(self, v1, v2):
-        
         self.edges.append(len(self.edges))
 
         if v1 == v2:
             print("Same vertex %d and %d" % (v1, v2))
+            return
         
         for i in range(len(self.incidence_matrix)):
             if i==v1 or i==v2:
                 self.incidence_matrix[i].append(1)
             else:
                 self.incidence_matrix[i].append(0)
-
-        #self.incidence_matrix[v1][v2] += 1
-        #self.incidence_matrix[v2][v1] += 1
-
+                
+    
+    def getEdgesAdjacency(self):
+        zipped_rows = zip(*self.incidence_matrix)
+        matriz_T = [list(row) for row in zipped_rows]
+        for i in range(len(self.edges)):
+            adj_edges = []
+            for j in range(len(matriz_T[i])):
+                if matriz_T[i][j] == 1:
+                    adj_edges += [e for e in range(len(self.incidence_matrix[j])) if self.incidence_matrix[j][e] == 1 and e not in adj_edges and e != i]
+            
+            self.edge_adjacency[i] = sorted(adj_edges)
 
 
     # Remove edges
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import time
 import random
 import networkx as nx
 import sys
+import getopt
 
 def getRandomVer(n):
     return [random.randint(0, 9) for i in range(n)]
@@ -11,19 +12,15 @@ def getRandomVer(n):
 def getRandomEdges(n, ver):
     pass
 
-def main(num_vertices):
+def main(num_vertices, search_type):
     graph = Graph(num_vertices)
     G = nx.Graph()
     G.add_nodes_from([x for x in range(0, num_vertices)])
 
-    x = getRandomVer(num_vertices)
-    y = getRandomVer(num_vertices)
-    print(x)
-    print(y)
     chosen_edges = {}
     begin = time.time()
     for i in range(0,num_vertices):
-            n_iters = random.choice([x for x in range(0,num_vertices)])
+            n_iters = random.choice([x for x in range(1,num_vertices)])
             used_vertices = []
             for iteration in range(0, n_iters):
                 j = []
@@ -46,24 +43,50 @@ def main(num_vertices):
                         chosen_edges[j_choice].add(i)
 
                     used_vertices.append(j_choice)
+    
+    """graph.add_edge(0,1)
+    graph.add_edge(1,2)
+    graph.add_edge(2,3)
+    graph.add_edge(3,4)
+    graph.add_edge(3,1)
+    G.add_edge(0,1)
+    G.add_edge(1,2)
+    G.add_edge(2,3)
+    G.add_edge(3,4)
+    G.add_edge(3,1)"""
             
     graph.printMatriz()
+    graph.getEdgesAdjacency()
     nx.draw(G, with_labels=True)
     
-    all_sol = graph.getAllSolutions()
+    #all_sol = graph.getAllSolutions()
+    if search_type == "greedy":
+        result = graph.findGreedySolution()
+        print("Minimum edge dominating set ({} algorithm): {}".format(search_type, result))
+    else:
+        result = graph.findExhaustiveSolution(graph.getAllSolutions())
+        print("Minimum edge dominating set ({} algorithm): {}".format(search_type, result))
     
-    graph.findGreedySolution()
-
-    #res = graph.findExhaustiveSolution(all_sol)
-    #print("Minimum edge dominating set: {}".format(res))
-    #print("Time: {}".format(time.time() - begin))
+    print("Time: {}".format(time.time() - begin))
     
     plt.show()
+
 if __name__ == "__main__":
     num_vertices = None
+    search_type = None      #  greedy | exhaustive
+
     try:
         num_vertices = int(sys.argv[1])
+        search_type = sys.argv[2]
     except Exception as err:
-        print("Usage: python3 main.py \n\t-v <generate random graph with N vertices: int>")
+        print("Usage: python3 main.py <generate random graph with N vertices (int)> <algorithm type (str: 'greedy' | str: 'exhaustive')>")
 
-    main(num_vertices)
+    if not isinstance(num_vertices, int):
+        print("Vertices not int!")
+        print("Usage: python3 main.py <generate random graph with N vertices (int)> <algorithm type (str: 'greedy' | str: 'exhaustive')>")
+        sys.exit(2)
+    
+    if num_vertices and (search_type == 'greedy' or search_type == 'exhaustive'):
+        main(num_vertices, search_type)
+    else:
+        print("Usage: python3 main.py <generate random graph with N vertices (int)> <algorithm type (str: 'greedy' | str: 'exhaustive')>")
